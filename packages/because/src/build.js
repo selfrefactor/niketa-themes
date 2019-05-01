d5bf1966bf8994b21fc5ef649c2051611ad3b655
Dejan Toteff <deyan8284@gmail.com>
2019-05-01 16:01:36 +0300

    feat@progress publish dark themes docs and homepage | only readme left

--- packages/because/src/build.js
+++ packages/because/src/build.js
@@ -1,12 +1,14 @@
-const { pascalCase, snakeCase } = require('string-fn')
-const { copySync } = require('fs-extra')
 const packageBase = require('./packageBase.json')
+const { change } = require('rambdax')
+const { copySync, outputJsonSync } = require('fs-extra')
 const { existsSync } = require('fs')
+const { pascalCase, snakeCase } = require('string-fn')
 const { resolve } = require('path')
 
 const base = resolve(__dirname, '../../')
 const themesFolder = resolve(__dirname, '../themes')
 const screensFolder = resolve(__dirname, '../screens')
+const packageJsonFile = resolve(__dirname, '../package.json')
 
 const THEMES = [
   'ask',
@@ -14,15 +16,15 @@ const THEMES = [
 ]
 
 function workingMan(label){
-  const pascalName = pascalCase(`because.${label}`)
-  const snakeName = snakeCase(`because.${label}`)
+  const pascalName = pascalCase(`because.${ label }`)
+  const snakeName = snakeCase(`because.${ label }`)
 
-  const themePath = `${base}/${snakeName}/theme/${pascalName}.json`
-  const themeOutput = `${themesFolder}/${pascalName}.json`
-  
-  const screenPath = `${base}/${snakeName}/theme/because.${label}.png`
-  const screenOutput = `${screensFolder}/because.${label}.png`
-  if(!existsSync(themePath)) throw new Error(`themePath - ${themePath}`)
+  const themePath = `${ base }/${ snakeName }/theme/${ pascalName }.json`
+  const themeOutput = `${ themesFolder }/${ pascalName }.json`
+
+  const screenPath = `${ base }/${ snakeName }/theme/because.${ label }.png`
+  const screenOutput = `${ screensFolder }/because.${ label }.png`
+  if (!existsSync(themePath)) throw new Error(`themePath - ${ themePath }`)
 
   copySync(
     themePath,
@@ -35,15 +37,22 @@ function workingMan(label){
   )
 
   return {
-      "label": pascalName,
-      "uiTheme": "vs-dark",
-      "path": `./themes/${pascalName}.json`
+    label   : pascalName,
+    uiTheme : 'vs-dark',
+    path    : `./themes/${ pascalName }.json`,
   }
 }
 
 function build(){
-  const result = THEMES.map(workingMan)
-  return result
+  const themes = THEMES.map(workingMan)
+  const toSave = change(
+    packageBase,
+    'contributes.themes',
+    themes
+  )
+
+  outputJsonSync(packageJsonFile, toSave, { spaces : 2 })
 }
+
 
 exports.build = build
