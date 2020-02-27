a4d4cb927617596bc0375ae95753b3bdcaaedcce
Dejan Toteff <deyan8284@gmail.com>
2020-02-27 10:28:28 +0200

    chore@dnd

--- packages/niketa_dark/src/createMultipleTheme.spec.js
+++ packages/niketa_dark/src/createMultipleTheme.spec.js
@@ -141,7 +141,7 @@ export const baseColors = {
   'editor.findMatchHighlightBackground'       : '#71aac355',
   'editor.findRangeHighlightBackground'       : '#3f706366',
   'editor.lineHighlightBackground'            : 'MAIN_COLOR_DARK',
-  'editor.lineHighlightBorder'                : '#c3c1a9',
+  'editor.lineHighlightBorder'                : '#fafafa',
   'editor.wordHighlightBackground'            : '#bacfd6',
   'editor.wordHighlightStrongBackground'      : '#7c8489',
   'editorBracketMatch.background'             : '#B1365B',
