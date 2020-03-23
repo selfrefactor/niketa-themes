96e7d3109b69ed77e1aba3a1f0a874cdcdd6b972
Dejan Toteff <deyan8284@gmail.com>
2020-03-23 22:57:20 +0200

    chore@dnd

--- packages/niketa_dark/src/createMultipleTheme.spec.js
+++ packages/niketa_dark/src/createMultipleTheme.spec.js
@@ -16,8 +16,8 @@ const listStandard = {
   // in the circle of unsaved changes
   // in extensions preview
   'foreground'                       : '#e7e7e7',
-  // ?
-  'list.dropBackground'              : '#bae637',
+  // on drag and drop of folders
+  'list.dropBackground'              : '#db82d6',
   // when select theme, this is active theme background
   'list.focusBackground'             : '#6d50a1',
   'list.highlightForeground'         : '#4d0e0b',
